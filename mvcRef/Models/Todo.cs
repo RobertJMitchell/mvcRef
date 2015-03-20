0e9938ffc4e1451faa64bda414eab34cac978645
RobertJMitchell <rjrocks32@gmail.com>
2015-03-20 10:31:47 -0500

    added new class for ToDo ViewModel

--- mvcRef/Models/Todo.cs
+++ mvcRef/Models/Todo.cs
@@ -5,11 +5,20 @@ using System.Web;
 
 namespace mvcRef.Models
 {
+    //Todos Model
     public class Todo
     {
+        public int Id { get; set; }
         //The Chore
         public string Task { get; set; }
         public DateTime dateCreated { get; set; }
         public bool isCompleted { get; set; }
+    }
+    //Todos Page ViewModel (VM)
+    public class TodoIndexVM
+    {
+        public string Greeting { get; set; }
+        public List<Todo> MyChores { get; set; }
+
     }
 }
